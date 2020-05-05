df722396877ef76ffd1a5701b96173f9b647b3fa
dx <aryadevchavali1@gmail.com>
2020-05-05 22:12:54 +0100

    +option parsing capability
    This allows for command line arguments. Currently have -i and -o:
    -i for initial template html
    -o for the output type (date or filename)

--- converter.py
+++ converter.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+from optparse import OptionParser
+"""Command line arguments"""
+usage = "usage: %prog [options] file..."
+parser = OptionParser(usage=usage)
+parser.add_option('-i', '--initial-html', dest='header',
+                  help='File for initial template of compiled HTML',
+                  default='initial.html')
+parser.add_option('-o', '--output-type', dest='output_type',
+                  help='Type of output (filename | date)',
+                  default='filename')
+(options, args) = parser.parse_args()
+
+
