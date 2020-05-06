3aef989bf97185e4c01084dc6b99aeafcca215ff
dx <aryadevchavali1@gmail.com>
2020-05-06 03:43:19 +0100

    +testing system
    Very elementary, doesn't use some massive framework, should just use
    asserts and C-lang constructs. Currently one method is present in the
    header, test_header_depths.

---
+++ Converter/includes/test.h
@@ -0,0 +1,8 @@
+#ifndef __TEST_H_
+#define __TEST_H_
+
+// Test methods to run
+
+void test_header_depths(void);
+
+#endif // __TEST_H_
---
+++ Converter/src/test.c
@@ -0,0 +1,9 @@
+#include "../includes/compiler.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+void test_header_depths(void)
+{
+
+}
