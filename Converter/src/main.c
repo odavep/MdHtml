ab7096fadb01cc3eb1a4d687ba5b4cbb326a0a8c
dx <aryadevchavali1@gmail.com>
2020-05-06 03:45:59 +0100

    +simple testing argument to executable in main
    If argc is greater than 1 and the second argument is --test then run tests.

--- Converter/src/main.c
+++ Converter/src/main.c
@@ -1,9 +1,17 @@
 #include "../includes/compiler.h"
+#include "../includes/test.h"
 #include <malloc.h>
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-  return 0;
+  if (argc > 1)
+  {
+    if (strncmp(argv[1], "--test", 7) == 0)
+    {
+      // run tests
+      test_header_depths();
+    }
+  }
 }
