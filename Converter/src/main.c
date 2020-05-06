ed3a856000cd1a5d51e7046b635df41e0ead9c5a
dx <aryadevchavali1@gmail.com>
2020-05-06 01:22:38 +0100

    +compile_line function, can turn '#' -> <h1>
    compile_line is the main function which compiles a single line in C.
    Similar to the stage of compilation in the python program.
    
    Currently supports header tags

--- Converter/src/main.c
+++ Converter/src/main.c
@@ -1,6 +1,33 @@
+#include <malloc.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef char *string;
+
+string compile_line(string line, size_t size_of_line)
+{
+  string compiled_line;
+  if (line[0] == '#')
+  {
+    // title
+    // allocate buffer with extra 9 characters for the tags
+    compiled_line = malloc(sizeof(*compiled_line) * (size_of_line + 9));
+    // write h1 to start of compiled_line
+    strncpy(compiled_line, "<h1>", 4);
+    // write the rest of the line to the compiled_line
+    strncpy(compiled_line + 4, line + 1, size_of_line - 1);
+    // write the end tags
+    strncpy(compiled_line + size_of_line + 3, "</h1>", 5);
+  }
+
+  else
+  {
+    compiled_line = malloc(sizeof(*compiled_line) * size_of_line);
+    strncpy(compiled_line, line, size_of_line);
+  }
+
+  return compiled_line;
+}
 
 int main()
 {
