9042af391b891b0b0b9d9a04d35ac98ea95ca11d
dx <aryadevchavali1@gmail.com>
2020-05-20 14:56:59 +0100

    ~!refactor of the compile_inner_text function
    Set compiled token recipes in the compiler.h file (easier to manage) and
    let code handle the rest.

--- Converter/includes/compiler.h
+++ Converter/includes/compiler.h
@@ -6,6 +6,8 @@
 
 typedef char *string;
 const static char TOKENS[] = {'*', '_', '~'};
+const static char *TOKENS_COMPILED[] = { "<i>", "<u>", "<s>" };
+const static char *TOKENS_CLOSE_COMPILED[] = { "</i>", "</u>", "</s>" };
 const static size_t N_TOKENS = 3;
 
 /* Compile markdown line to HTML line */
@@ -14,5 +16,7 @@ string compile_line(string line, size_t sz_line, string line_or_source);
 int find_string(string src, string query, size_t sz_src, size_t sz_query);
 /* Check if a given character is a text token*/
 bool is_token(char c);
+/* Generate the compiled token pair for a given single sized markdown token */
+char **get_token_pair(char c);
 
 #endif // __COMPILER_H_
